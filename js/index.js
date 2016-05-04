65dca8d4a5936229c64d0f42a0301c6b255a56b1
knaggs <knad1_14@uni.worc.ac.uk>
2016-05-04 09:29:00 +0100

    Add checkconnection function

--- js/index.js
+++ js/index.js
@@ -15,9 +15,31 @@ function onOffline() {
 }
     
 
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
+
+
+
+
+
 $(document).on('pagecreate', '#feedPage', function(event) {
 	
 	
+	checkConnection();
 	// <!-- Use an HTML GET request to obtain data from a Yahoo Pipe
 	// <!-- The Yahoo pipe currently parses the BBC News RSS feed  -->
 	var xmlhttp=new XMLHttpRequest();
