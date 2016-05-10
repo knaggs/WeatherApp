111261863fafb24bd9c3698b335f2af8d1181dda
knaggs <knad1_14@uni.worc.ac.uk>
2016-05-10 09:11:53 +0100

    Update check internet connection with toast

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 var feedURL = "https://www.metaweather.com/api/location/44418/";
 
+if (doesConnectionExist() == true) {
+			new Android_Toast({content: 'Working..', duration: 2000, position: 'top'});
+		} else {
+			new Android_Toast({content: 'Please connect to the internet!', duration: 6000, position: 'top'});
+		}
+
 function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady(){
 	console.log('OndeviceReady running');
-	document.addEventListener("onoffline", onOffline, false);
-}
-
-function onOffline() {
-	console.log('onOffline running');
-   	new Android_Toast({content: 'Please connect to the internet!', duration: 6000, position: 'top'}); // Handle the offline event
 }
 
 function doesConnectionExist() {
@@ -33,23 +33,6 @@ function doesConnectionExist() {
         return false;
     }
 }
-
-function checkConnection() {
-    var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
-}
-
 
 $(document).on('pagecreate', '#feedPage', function(event) {
 	
